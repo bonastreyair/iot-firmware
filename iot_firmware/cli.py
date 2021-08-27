29f6c51b7285d74a051c08b7363888f19daa147d
bonastreyair <bonastreyair@gmail.com>
2021-08-28 00:15:44 +0200

    aded cli, improved pre-commit, added __version__, removed requirements, remove check_tag

---
+++ iot_firmware/cli.py
@@ -0,0 +1,22 @@
+import argparse
+
+from . import __version__
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="iot-firmware",
+    )
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version=f"{parser.prog} {__version__}",
+        help="show program's version number and exit",
+    )
+    parser.add_argument(
+        "-c",
+        "--config",
+        help="path to the configuration",
+    )
+    parser.parse_args()
